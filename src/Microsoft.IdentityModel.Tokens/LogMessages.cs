3d3421dd651356e091d43264a28bf381e1bc3f3c
Maria Furman <mafurman@microsoft.com>
2019-11-12 15:08:46 -0800

    Some small grammar fixes in the comments and log messages.

--- src/Microsoft.IdentityModel.Tokens/LogMessages.cs
+++ src/Microsoft.IdentityModel.Tokens/LogMessages.cs
@@ -207,7 +207,7 @@ namespace Microsoft.IdentityModel.Tokens
         public const string IDX10706 = "IDX10706: Cannot create a JWK thumbprint, '{0}' must be one of the following: '{1}'.";
         public const string IDX10707 = "IDX10707: Cannot create a JSON representation of an asymmetric public key, '{0}' must be one of the following: '{1}'.";
         public const string IDX10708 = "IDX10708: Cannot create a JSON representation of an EC public key, '{0}' is null or empty.";
-        public const string IDX10709 = "IDX10709: Cannot create a JSON representation of an RSA public key, '{0}' is null or empty.";
+        public const string IDX10709 = "IDX10709: Cannot create a JSON representation of a RSA public key, '{0}' is null or empty.";
 
         // Json specific errors
         //public const string IDX10801 = "IDX10801:"
